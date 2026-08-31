2fc7ce6d46387c1e14aa8fb101ce1624a918074e
tereshchenkodamir <damirteresenko81@gmail.com>
2026-09-01 00:52:08 +0300

    Small typo fixes + the program can now retrieve and print user's top tracks

--- project.py
+++ project.py
@@ -6,10 +6,11 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 api_key = os.getenv("LASTFM_API_KEY")
-url = "http://ws.audioscrobbler.com/2.0/"
+url = "https://ws.audioscrobbler.com/2.0/"
 
 def main():
     username = input("Enter Last.fm username: ")
+
 
     try:
         top_artists_data = get_top_artists(username)
@@ -18,15 +19,31 @@ def main():
         return
 
     artists = top_artists_data["topartists"]["artist"]
+    artist_plays = {artist["name"]: int(artist["playcount"]) for artist in artists} 
 
-    artist_plays = {artist["name"]: artist["playcount"] for artist in artists} 
+
     for artist, plays in artist_plays.items():
         print(f"{artist}: {plays} plays")
 
-    print("\nData provided by Last.fm")
-    print("https://www.last.fm/\n")
+
+###############################################################################
+
+
+    try:
+        data_top_tracks = get_top_tracks(username)
+    except HTTPError as e:
+        print(f"Error: {e}")
+        return
+    
+    top_tracks = data_top_tracks["toptracks"]["track"]
+    top_tracks_plays = {track["name"]: int(track["playcount"]) for track in top_tracks}
+
+    for track, plays in top_tracks_plays.items():
+            print(f"{track}: {plays} plays")
     
 
+    print("\nData provided by Last.fm")
+    print("https://www.last.fm/\n")
     
 def get_top_artists(username):
     params = {
@@ -43,7 +60,20 @@ def get_top_artists(username):
 
     return response.json()
     
-
+def get_top_tracks(username):
+    params = {
+            "method": "user.getTopTracks",
+            "user": username,
+            "api_key": api_key,
+            "format": "json",
+            "limit": 10
+        } 
+    
+    response = requests.get(url=url, params=params, timeout=10)
+    
+    response.raise_for_status()
+    
+    return response.json()
 
 if __name__ == "__main__":
     main()
